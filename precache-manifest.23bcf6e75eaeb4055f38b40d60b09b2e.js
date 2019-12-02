06d6829e9f72243c26f3d5942c04a1867390c03f
Samson Kolge <samson.kolge@walmartlabs.com>
2019-12-02 19:13:24 +0530

    Updates

---
+++ precache-manifest.23bcf6e75eaeb4055f38b40d60b09b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3c40781b1bd0b5e3f1add70115b584",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "df975f536dcdfc54f262",
+    "url": "/monsters-rolodex/static/css/main.8c40280a.chunk.css"
+  },
+  {
+    "revision": "8141f0803164906a6a51",
+    "url": "/monsters-rolodex/static/js/2.fd9aaf2d.chunk.js"
+  },
+  {
+    "revision": "df975f536dcdfc54f262",
+    "url": "/monsters-rolodex/static/js/main.de212323.chunk.js"
+  },
+  {
+    "revision": "82d6f8080f7412f410fc",
+    "url": "/monsters-rolodex/static/js/runtime-main.b56caa74.js"
+  }
+]);
